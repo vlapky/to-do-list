ff6fd6126c49e79feb10afec85b1a99bbf268fc6
vlapky <snkreya@gmail.com>
2020-11-24 00:36:01 +0300

    design and add tasks functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
+import Input from './Input';
+import Item from './Item';
+import Header from './Header';
+
+import './normalize.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: '',
+      list: []
+    }
+  }
+  onChange = ({ target: { value } }) => {
+    this.setState({ inputText: value });
+  }
+  onKeyPress = ({key}) => {
+    if(key==='Enter') {
+      this.addItem();
+    }
+  }
+  addItem = () => {
+    if(this.state.inputText.length > 0) {
+      this.setState((state) => {
+        const list = [...state.list, {id: new Date(), text: state.inputText, isCompleted: false}];
+        return {list, inputText: ''};
+      });
+    }
+  }
+  deleteItem = ({key}) => {
+    this.setState((state) => {
+      const list = state.list.filter(({id})=>{
+        return id !== key;
+      })
+      return list;
+    });
+  }
+  render() {
+
+    const { inputText, list } = this.state;
+
+    return (
+      <div className="App">
+        <div className='todo'>
+          <Header title='Список задач' />
+          <Input onKeyPress={this.onKeyPress} addItem={this.addItem} value={inputText} onChange={this.onChange} />
+          {list.map(({ id, text, isCompleted }) => 
+          <Item 
+          id={id} 
+          text={text} 
+          isCompleted={isCompleted}
+          delete={this.deleteItem}
+           />)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+const Header = ({title}) => {
+    return (
+        <div className="header">
+            <p>{title}</p>
+        </div>
+    )
+}
+
+export default Header;
